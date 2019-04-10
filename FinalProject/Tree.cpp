c60ee75c236c5e24e7161d4e907cb39301cfbc7a
Tom <tom.lucero@me.com>
2019-04-10 01:41:35 -0600

    added comments to Insert functions

--- FinalProject/Tree.cpp
+++ FinalProject/Tree.cpp
@@ -20,6 +20,7 @@ void Tree::setRootPtr(Node &node){
 
 void Tree::InsertNode(Data _data, string _key){
 
+    //inserts node if rootptr is empty
     if(rootPtr == nullptr){
         Node* NewNode = new Node;
         NewNode -> SetKey(_key);
@@ -29,6 +30,7 @@ void Tree::InsertNode(Data _data, string _key){
         
     }
     else{
+        //calls overloaded insertNode function with rootPtr 
         InsertNode(_data, rootPtr, _key);
     }
 }
@@ -39,7 +41,9 @@ void Tree::InsertNode(Data _data, Node *root, string key){
 //        cout << "value exists.." << endl; //Should we have error checking if key == root key? May cause duplicates.. right now it is unsorted. IE. Ruth Catterson 1929 and 1930.
     }
     
+    //if key is less than the root key, insert as left child
     if(key < root -> GetKey()){
+        //if left node is empty, insert node.
         if (root -> GetLeftNode() == nullptr){
             Node* LeftChild = new Node;
             LeftChild -> SetData(_data);
@@ -47,18 +51,20 @@ void Tree::InsertNode(Data _data, Node *root, string key){
             root -> setLeftNode(LeftChild);
         }
         else{
+            //if left child is not empty, calls overloaded InsertNode function
             InsertNode(_data, root -> GetLeftNode(), key);
         }
     }
     else{
+        //if right node is empty, insert as right child
         if (root -> GetRightNode() == nullptr){
             Node* RightChild = new Node;
-            
             RightChild -> SetData(_data);
             RightChild -> SetKey(key);
             root -> setRightNode(RightChild);
         }
         else{
+            //if right child is not empty, calls overloaded InsertNode function
             InsertNode(_data, root -> GetRightNode(), key);
         }
     }
